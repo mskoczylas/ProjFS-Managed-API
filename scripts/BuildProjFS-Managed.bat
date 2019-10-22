8cf6cb737996fab8f2a0053cc5d7cf4aae198b15
Mariusz Skoczylas <mskoczylas@abilis-solutions.com>
2019-10-22 13:20:13 -0400

    debug

--- scripts/BuildProjFS-Managed.bat
+++ scripts/BuildProjFS-Managed.bat
@@ -50,11 +50,11 @@ IF NOT EXIST %msbuild% (
 
 :: Restore all dependencies.
 echo Step1
-%nuget% restore %PROJFS_SRCDIR%\ProjectedFSLib.Managed.sln -MSBuildPath '%VsInstallDir%'
-echo Step2
-"%VsInstallDir%\Common7\Tools\VsDevCmd.bat"&dotnet restore %PROJFS_SRCDIR%\ProjectedFSLib.Managed.sln /p:Configuration=%SolutionConfiguration% /p:VCTargetsPath="C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V140" --packages %PROJFS_PACKAGESDIR% || exit /b 1
-echo Step3
-:: Kick off the build.
-"%VsInstallDir%\Common7\Tools\VsDevCmd.bat"&msbuild %PROJFS_SRCDIR%\ProjectedFSLib.Managed.sln /p:ProjFSManagedVersion=%ProjFSManagedVersion% /p:Configuration=%SolutionConfiguration% /p:Platform=x64 /p:PlatformToolset=!PlatformToolset! || exit /b 1
+%nuget% restore %PROJFS_SRCDIR%\ProjectedFSLib.Managed.sln -MSBuildPath "%VsInstallDir%\MSBuild\Current\Bin"
+echo Step2 "%VsInstallDir%\Common7\Tools\VsDevCmd.bat"&
+dotnet restore %PROJFS_SRCDIR%\ProjectedFSLib.Managed.sln /p:Configuration=%SolutionConfiguration% /p:VCTargetsPath="C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V140" --packages %PROJFS_PACKAGESDIR% || exit /b 1
+echo Step3 "%VsInstallDir%\Common7\Tools\VsDevCmd.bat"&
+:: Kick off the build. 
+!msbuild! %PROJFS_SRCDIR%\ProjectedFSLib.Managed.sln /p:ProjFSManagedVersion=%ProjFSManagedVersion% /p:Configuration=%SolutionConfiguration% /p:Platform=x64 /p:PlatformToolset=!PlatformToolset! || exit /b 1
 echo Step4
 ENDLOCAL
